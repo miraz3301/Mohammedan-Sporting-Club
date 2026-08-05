890b6374901803c2c24a25b927095baea84661e5
Md. Miraz Morshed <miraz.rzsian21@gmail.com>
2026-08-05 06:22:51 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+// Fade-in animation
+window.addEventListener("load", () => {
+    document.body.classList.add("loaded");
+});
+
+// Smooth page transition
+document.querySelectorAll("a").forEach(link => {
+
+    if (
+        link.hostname === window.location.hostname &&
+        !link.hasAttribute("target") &&
+        link.href !== ""
+    ) {
+
+        link.addEventListener("click", function (e) {
+
+            e.preventDefault();
+
+            document.body.classList.remove("loaded");
+
+            setTimeout(() => {
+                window.location.href = this.href;
+            }, 350);
+
+        });
+
+    }
+
+});
+
+// Login Form
+const loginForm = document.getElementById("loginForm");
+
+if(loginForm){
+
+    loginForm.addEventListener("submit",function(e){
+
+        e.preventDefault();
+
+        const username=document.getElementById("username").value;
+        const password=document.getElementById("password").value;
+
+        // Demo login
+        if(username==="admin" && password==="1234"){
+
+            document.body.classList.remove("loaded");
+
+            setTimeout(()=>{
+
+                window.location.href="dashboard.html";
+
+            },350);
+
+        }
+
+        else{
+
+            alert("Invalid Username or Password");
+
+        }
+
+    });
+
+}
+const buttons=document.querySelectorAll(".tab-btn");
+const cards=document.querySelectorAll(".player-card");
+
+buttons.forEach(button=>{
+
+    button.addEventListener("click",function(){
+
+        buttons.forEach(btn=>btn.classList.remove("active"));
+
+        this.classList.add("active");
+
+        const category=this.dataset.category;
+
+        cards.forEach(card=>{
+
+            if(category==="all" || card.classList.contains(category))
+            {
+                card.style.display="block";
+            }
+            else
+            {
+                card.style.display="none";
+            }
+
+        });
+
+    });
+
+});
